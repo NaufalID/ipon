ea4a9b4bc1369ebb30685d991b0c184279a45dbc
NaufalID <88891630+NaufalID@users.noreply.github.com>
2023-03-21 20:54:53 +0700

    Update anime-anna.js

--- plugins/anime-anna.js
+++ plugins/anime-anna.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://api.xhitori.repl.co/api/sfw/anna?apikey=miracle'
+	let url = 'https://raw.githubusercontent.com/NaufalID/asupan2/master/ana.json'
 	conn.sendButton(m.chat, 'Waifu nya om (≧ω≦)', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
 }
 handler.command = /^(anna)$/i
