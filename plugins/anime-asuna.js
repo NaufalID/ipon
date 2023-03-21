345f0fdd78459bf3e3d36d5aff734f815c823f3f
NaufalID <88891630+NaufalID@users.noreply.github.com>
2023-03-21 20:55:41 +0700

    Update anime-asuna.js

--- plugins/anime-asuna.js
+++ plugins/anime-asuna.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://revita.herokuapp.com/api/wallpaper/asuna?apikey=ApiRevita'
+	let url = 'https://raw.githubusercontent.com/NaufalID/asupan2/master/asuna.json'
 	conn.sendButton(m.chat, 'Waifu nya om (≧ω≦)', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
 }
 handler.command = /^(asuna)$/i
